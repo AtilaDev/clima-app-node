bbc41757419d6725f67929786e49baf62376494d
Leandro Favre <atiladevelop@gmail.com>
2018-09-20 08:28:47 -0300

    Update clima.js

--- clima/clima.js
+++ clima/clima.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const getClima = async (lat, lng) => {
   let resp = await axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=baf8346ba1e8adbc8ca10380362b7a17`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid={API_KEY}`
   );
 
   if (resp.data.cod === 400) {
